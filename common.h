1b4e5074437a525be77061c106aed207b97feddc
Gunnar Karlsson <gunnar.h.karlsson@gmail.com>
2018-09-14 21:51:40 +0800

    Initial commit

---
+++ common.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
+
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#define DEBUG_TAG "EndlessTunnel:Native"
+
+#define BUFFER_OFFSET(i) ((char*)NULL + (i))
+
+#endif // COMMON_H
